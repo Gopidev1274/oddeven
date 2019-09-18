30d06756e0bed067c4ce2ab30b70c435740ae899
Gopidev1274 <gopigamer.1274@gmail.com>
2019-09-18 13:24:55 +0530

    success

---
+++ src/org/gcmds/Day1.java
@@ -0,0 +1,14 @@
+package org.gcmds;
+
+public class Day1 {
+public static void main(String[] args) {
+	int a=0;
+	if(a%2==1) {
+		System.out.println("Odd number");
+	}
+	else {
+		System.out.println("Even no");
+	}
+}
+
+}
